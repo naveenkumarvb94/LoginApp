0168da775961f6ae451ba0d02c6e365bfa3760f9
naveenkumarvb94 <49961986+naveenkumarvb94@users.noreply.github.com>
2019-04-25 03:32:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import { createStore } from 'redux'
-import Login from './src/app'
+import Login from './src/app/index.js'
 import rootReducer from './src/redux/reducers'
 
 const store = createStore(rootReducer)
